890140d9b28525f5d485e8298c4f5bd2f1ed14ce
Pau1iv <117527852+Pau1iv@users.noreply.github.com>
2024-01-20 17:44:55 +0100

    tab ramka

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def tabliczka_ramka(rozmiar):
+    for i in range(1,rozmiar+1):
+        for j in range (1,rozmiar+1):
+            if i==1 or j==1 or i==rozmiar or j==rozmiar:
+                print("*")
+            else:
+                print(" ")
+        print()
+
+
+
+
+
 def tabliczka_mnozenia(rozmiar):
     for i in range(1, rozmiar + 1):
         for j in range(1, rozmiar + 1):
@@ -8,3 +21,4 @@ def tabliczka_mnozenia(rozmiar):
 
 rozmiar_tabliczki: int = 10
 tabliczka_mnozenia(rozmiar_tabliczki)
+tabliczka_ramka(rozmiar_tabliczki)
