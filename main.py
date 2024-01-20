034362cd1c706aff759b22b854fa28f9eeac8bf7
Pau1iv <117527852+Pau1iv@users.noreply.github.com>
2024-01-20 17:37:20 +0100

    tab gwiazdki

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def tabliczka_mnozenia(rozmiar):
     for i in range(1, rozmiar + 1):
         for j in range(1, rozmiar + 1):
-            wynik = i * j
+            wynik = i + j
             print(f'{i} * {j} = {wynik}', end='\t')
         print()
+def tabliczka_gwiazdki(rozmiar):
+    for i in range(1,rozmiar+1):
+        for j in range(1,rozmiar+1):
+            print("*")
+        print()
 
-
-rozmiar_tabliczki: int = 10
+rozmiar_tabliczki: int = 5
 tabliczka_mnozenia(rozmiar_tabliczki)
+tabliczka_gwiazdki(rozmiar_tabliczki)
